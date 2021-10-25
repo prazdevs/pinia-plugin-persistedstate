50b938029c397ce00bb67e5602eff0aa719763fd
PraZ <sachabouillez@gmail.com>
2021-10-25 22:41:02 +0200

    :sparkles: add vuex-persistedstate like options

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,51 @@
 import { PiniaPluginContext } from 'pinia'
+import * as shvl from 'shvl'
 
 declare module 'pinia' {
-  export interface DefineStoreOptions<Id extends string, S extends StateTree,  G,  A> {
-    persist?: boolean
+  export interface DefineStoreOptions<Id extends string, S extends StateTree, G, A> {
+    persist?: boolean | PersistedStateOptions
   }
 }
 
+export type StorageLike = Pick<Storage, 'getItem' | 'removeItem' | 'setItem'>
+
+export interface PersistedStateOptions {
+  key?: string
+  storage?: StorageLike
+  paths?: Array<string>
+  overwrite?: boolean
+}
+
 export default function (context: PiniaPluginContext): void {
-  if (context.options.persist) {
-    const key = context.store.$id
+  const { options: { persist }, store } = context
 
-    const fromStorage = localStorage.getItem(key)
+  if (!persist) return
 
-    if (fromStorage) context.store.$patch(JSON.parse(fromStorage))
+  const {
+    storage = localStorage,
+    key = store.$id,
+    paths = null,
+    overwrite = false,
+  } = typeof persist != 'boolean' ? persist : {}
 
-    context.store.$subscribe((_: any, state: any) => {
-        localStorage.setItem(key, JSON.stringify(state))
-      },
-    )
-  }
+  try {
+    const fromStorage = storage.getItem(key)
+    if (fromStorage) {
+      if (overwrite) store.$state = JSON.parse(fromStorage)
+      else store.$patch(JSON.parse(fromStorage))
+    } 
+  } catch (_) { }
+  
+  store.$subscribe((_: unknown, state: unknown) => {
+    try {
+      const toStore = Array.isArray(paths)
+        ? paths.reduce((substate, path) => {
+            return shvl.set(substate, path, shvl.get(state as Object, path))
+          }, {})
+        : state
+
+      storage.setItem(key, JSON.stringify(toStore))
+    } catch (_) { }
+    
+  })
 }
