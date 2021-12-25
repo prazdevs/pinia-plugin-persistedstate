04a70bfc82c664ab238fbb812540961928878c35
prazdevs <sachabouillez@gmail.com>
2021-12-25 19:47:56 +0100

    :construction: add vue flags to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  define: {
+    __VUE_OPTIONS_API__: 'true',
+    __VUE_PROD_DEVTOOLS__: 'false',
+  },
   test: {
     environment: 'happy-dom',
     setupFiles: ['./tests/setup.ts'],
     watch: false,
+    deps: { inline: ['vue2', '@vue/composition-api', 'vue-demi'] },
   },
 })
