611290d68f93278bc511812510931c003a511e67
prazdevs <sachabouillez@gmail.com>
2022-01-21 10:26:39 +0100

    :label: remove ts-ignore on Vue2 global props

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,16 +1,11 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-
 // yoinked from https://github.com/vueuse/vueuse/blob/main/packages/.test/index.ts
 
 import { beforeEach, vi } from 'vitest'
 import { install, isVue2, Vue2 } from 'vue-demi'
 
 if (isVue2) {
-  // @ts-ignore
   Vue2.config.productionTip = false
-  // @ts-ignore
   Vue2.config.devtools = false
-
   install(Vue2)
 }
 
