468f30213a012a313821939928f12dd953830f92
prazdevs <sachabouillez@gmail.com>
2022-03-02 13:57:37 +0100

    :bulb: add vue example

---
+++ examples/vue/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useUserStore } from './stores/user'
+
+const userStore = useUserStore()
+</script>
+
+<template>
+  <div>
+    <input v-model="userStore.username" />
+  </div>
+</template>
