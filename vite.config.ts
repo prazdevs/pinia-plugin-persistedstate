0744fb5454168971d4154e51837470910bada3b3
PraZ <sachabouillez@gmail.com>
2022-05-12 23:40:55 +0200

    :construction_worker: remove useless options

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  define: {
-    __VUE_OPTIONS_API__: 'true',
-    __VUE_PROD_DEVTOOLS__: 'false',
-  },
   test: {
     environment: 'happy-dom',
-    silent: true,
     coverage: {
       reporter: ['lcov', 'text'],
       include: ['src/plugin.ts', 'src/pick.ts', 'src/normalize.ts'],
