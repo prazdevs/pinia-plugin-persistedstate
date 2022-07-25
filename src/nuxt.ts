b8ad2ffd5fe4bf7e89e3a788a2ee3c5cbd8ed5ba
unknown <sachabouillez@gmail.com>
2022-07-26 01:52:17 +0200

    :truck: move pinia type extensions to core/types

--- src/nuxt.ts
+++ src/nuxt.ts
@@ -2,29 +2,7 @@ import type { CookieParseOptions, CookieSerializeOptions } from 'cookie-es'
 import type { Ref } from 'vue'
 
 import { createPersistedState } from '.'
-import {
-  PersistedStateFactoryOptions,
-  PersistedStateOptions,
-} from './core/types'
-
-declare module 'pinia' {
-  export interface DefineStoreOptionsBase<S extends StateTree, Store> {
-    /**
-     * Persist store in storage.
-     * @see https://github.com/prazdevs/pinia-plugin-persistedstate
-     */
-    persist?: boolean | PersistedStateOptions | PersistedStateOptions[]
-  }
-
-  export interface PiniaCustomProperties {
-    /**
-     * Rehydrates store from persisted state
-     * Warning: this is for advances usecases, make sure you know what you're doing.
-     * @see https://github.com/prazdevs/pinia-plugin-persistedstate
-     */
-    $hydrate: (opts?: { runHooks?: boolean }) => void
-  }
-}
+import { PersistedStateFactoryOptions } from './core/types'
 
 interface CookieOptions<T = any>
   extends Omit<
