9e679675a5deb71447833a7b14b6b8172663496d
unknown <sachabouillez@gmail.com>
2022-07-25 12:52:49 +0200

    :bug: fix nuxt exports

--- src/nuxt.ts
+++ src/nuxt.ts
@@ -2,7 +2,29 @@ import type { CookieParseOptions, CookieSerializeOptions } from 'cookie-es'
 import type { Ref } from 'vue'
 
 import { createPersistedState } from '.'
-import { PersistedStateFactoryOptions } from './core/types'
+import {
+  PersistedStateFactoryOptions,
+  PersistedStateOptions,
+} from './core/types'
+
+declare module 'pinia' {
+  export interface DefineStoreOptionsBase<S extends StateTree, Store> {
+    /**
+     * Persist store in storage.
+     * @see https://github.com/prazdevs/pinia-plugin-persistedstate
+     */
+    persist?: boolean | PersistedStateOptions | PersistedStateOptions[]
+  }
+
+  export interface PiniaCustomProperties {
+    /**
+     * Rehydrates store from persisted state
+     * Warning: this is for advances usecases, make sure you know what you're doing.
+     * @see https://github.com/prazdevs/pinia-plugin-persistedstate
+     */
+    $hydrate: (opts?: { runHooks?: boolean }) => void
+  }
+}
 
 interface CookieOptions<T = any>
   extends Omit<
