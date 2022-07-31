4e98ecbb4c35bf008ba4519ebfb460775c9bf2bc
prazdevs <sachabouillez@gmail.com>
2022-07-31 15:09:02 +0200

    :bug: fix type imports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,25 @@
+import type { PersistedStateOptions } from '~/core/types'
+
 import { createPersistedState } from '~/core/plugin'
+
+declare module 'pinia' {
+  export interface DefineStoreOptionsBase<S extends StateTree, Store> {
+    /**
+     * Persist store in storage.
+     * @see https://github.com/prazdevs/pinia-plugin-persistedstate
+     */
+    persist?: boolean | PersistedStateOptions | PersistedStateOptions[]
+  }
+
+  export interface PiniaCustomProperties {
+    /**
+     * Rehydrates store from persisted state
+     * Warning: this is for advances usecases, make sure you know what you're doing.
+     * @see https://github.com/prazdevs/pinia-plugin-persistedstate
+     */
+    $hydrate: (opts?: { runHooks?: boolean }) => void
+  }
+}
 
 export type {
   PersistedStateOptions,
--- src/nuxt.ts
+++ src/nuxt.ts
@@ -1,8 +1,11 @@
 import type { CookieParseOptions, CookieSerializeOptions } from 'cookie-es'
 import type { Ref } from 'vue'
+import type {
+  PersistedStateFactoryOptions,
+  PersistedStateOptions,
+} from '~/core/types'
 
 import { createPersistedState } from '~/core/plugin'
-import { PersistedStateFactoryOptions } from '~/core/types'
 
 interface CookieOptions<T = any>
   extends Omit<
@@ -20,6 +23,25 @@ type UseCookie<T = string> = (
   name: string,
   _opts?: CookieOptions<T>,
 ) => CookieRef<T>
+
+declare module 'pinia' {
+  export interface DefineStoreOptionsBase<S extends StateTree, Store> {
+    /**
+     * Persist store in storage.
+     * @see https://github.com/prazdevs/pinia-plugin-persistedstate
+     */
+    persist?: boolean | PersistedStateOptions | PersistedStateOptions[]
+  }
+
+  export interface PiniaCustomProperties {
+    /**
+     * Rehydrates store from persisted state
+     * Warning: this is for advances usecases, make sure you know what you're doing.
+     * @see https://github.com/prazdevs/pinia-plugin-persistedstate
+     */
+    $hydrate: (opts?: { runHooks?: boolean }) => void
+  }
+}
 
 export type PersistedStateNuxtFactoryOptions = Omit<
   PersistedStateFactoryOptions,
