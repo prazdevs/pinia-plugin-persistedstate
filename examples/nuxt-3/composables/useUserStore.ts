c2f77a209596fb7e42ecf71415aaf47ab19beace
prazdevs <sachabouillez@gmail.com>
2022-08-02 10:48:10 +0200

    :memo: update nuxt3 example

--- examples/nuxt-3/composables/useUserStore.ts
+++ examples/nuxt-3/composables/useUserStore.ts
@@ -1,5 +1,3 @@
-import { defineStore } from 'pinia'
-
 export const useUserStore = defineStore('user', {
   state: () => ({
     username: 'PraZ',
--- examples/nuxt-3/nuxt.config.ts
+++ examples/nuxt-3/nuxt.config.ts
@@ -1,6 +1,7 @@
 import { defineNuxtConfig } from 'nuxt'
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@pinia/nuxt']
+  modules: [
+    ['@pinia/nuxt', { autoImports: ['defineStore'] }],
+  ],
 })
