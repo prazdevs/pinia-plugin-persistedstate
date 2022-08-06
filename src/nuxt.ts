18bf3f69292175b19c3fbe02ce0d89fcade4e06b
Stephen Jason Wang <stephenjasonwang@gmail.com>
2022-08-06 19:58:56 +0800

    :sparkles: added support to split into multiple cookies & all storage driver that support get() & set()
    -  BREAKING CHANGE: removed support for useCookie
    -  RECOMMENDED ALTERNATIVE: cookie-universal or js-cookie

--- src/nuxt.ts
+++ src/nuxt.ts
@@ -1,14 +1,12 @@
-import { useCookie, CookieOptions } from 'nuxt/app'
+import { CookieOptions } from 'nuxt/app'
 
 import { createPersistedState } from '~/core/plugin'
 import { PersistedStateFactoryOptions } from '~/core/types'
 
-type UseCookie = typeof useCookie
-
 export type PersistedStateNuxtFactoryOptions = Omit<
   PersistedStateFactoryOptions,
   'storage'
-> & { cookieOptions?: CookieOptions<string> }
+> & { cookieOptions?: CookieOptions<string> } & { splitCookies: number }
 
 /**
  * Creates a Nuxt-specific SSR-ready pinia persistence plugin based on cookies
@@ -16,25 +14,46 @@ export type PersistedStateNuxtFactoryOptions = Omit<
  * @param factoryOptions global persistence options
  * @returns pinia plugin
  */
+
+interface UseCookie {
+  get: (key: string) => string | undefined
+  set: (key: string, value: string, options?: CookieOptions<string>) => void
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export function createNuxtPersistedState(
   useCookie: UseCookie,
   factoryOptions?: PersistedStateNuxtFactoryOptions,
 ) {
+  const handleCookie = (
+    data: UseCookie['get'] | UseCookie['set'],
+    key: string,
+    value = '',
+    cookie = '',
+  ) => {
+    const numberOfCookies = factoryOptions?.splitCookies
+      ? factoryOptions?.splitCookies
+      : 1
+    for (let i = 0; i < numberOfCookies; i++)
+      if (value) {
+        const length = Math.ceil(value.length / numberOfCookies)
+        const start = i * length
+        const end = start + length
+        data(key + (i || ''), value.toString().substring(start, end), {
+          ...factoryOptions?.cookieOptions,
+        })
+        // @ts-ignore
+      } else cookie += data(key + (i || ''))
+
+    return cookie
+  }
   return createPersistedState({
     storage: {
       getItem: key => {
-        return useCookie(key, {
-          encode: encodeURIComponent,
-          decode: decodeURIComponent,
-          ...factoryOptions?.cookieOptions,
-        }).value
+        return handleCookie(useCookie.get, key)
       },
       setItem: (key, value) => {
-        useCookie(key, {
-          encode: encodeURIComponent,
-          decode: decodeURIComponent,
-          ...factoryOptions?.cookieOptions,
-        }).value = value
+        handleCookie(useCookie.set, key, value)
       },
     },
     ...factoryOptions,
