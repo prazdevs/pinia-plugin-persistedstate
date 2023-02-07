74eec7de00d313158d955fe4bdfd845198279069
prazdevs <1631886+prazdevs@users.noreply.github.com>
2023-02-07 14:55:12 +0100

    :label: improve types and type docs

--- packages/plugin/src/types.ts
+++ packages/plugin/src/types.ts
@@ -1,5 +1,7 @@
 import { type PiniaPluginContext, type StateTree } from 'pinia'
 
+type Prettify<T> = { [K in keyof T]: T[K] }
+
 export type StorageLike = Pick<Storage, 'getItem' | 'setItem'>
 
 export interface Serializer {
@@ -59,7 +61,7 @@ export interface PersistedStateOptions {
   debug?: boolean
 }
 
-export type PersistedStateFactoryOptions = Pick<
+export type PersistedStateFactoryOptions = Prettify<Pick<
   PersistedStateOptions,
   'storage' | 'serializer' | 'afterRestore' | 'beforeRestore' | 'debug'
 > & {
@@ -74,13 +76,13 @@ export type PersistedStateFactoryOptions = Pick<
    * @default false
    */
   auto?: boolean
-}
+}>
 
 declare module 'pinia' {
   export interface DefineStoreOptionsBase<S extends StateTree, Store> {
     /**
      * Persists store in storage.
-     * @see https://github.com/prazdevs/pinia-plugin-persistedstate
+     * @see https://prazdevs.github.io/pinia-plugin-persistedstate
      */
     persist?: boolean | PersistedStateOptions | PersistedStateOptions[]
   }
@@ -89,10 +91,15 @@ declare module 'pinia' {
     /**
      * Rehydrates store from persisted state
      * Warning: this is for advances usecases, make sure you know what you're doing.
-     * @see https://github.com/prazdevs/pinia-plugin-persistedstate
+     * @see https://prazdevs.github.io/pinia-plugin-persistedstate/guide/advanced.html#forcing-the-rehydration
      */
     $hydrate: (opts?: { runHooks?: boolean }) => void
 
+    /**
+     * Persists store into configured storage
+     * Warning: this is for advances usecases, make sure you know what you're doing.
+     * @see https://prazdevs.github.io/pinia-plugin-persistedstate/guide/advanced.html#forcing-the-persistence
+     */
     $persist: () => void
   }
 }
