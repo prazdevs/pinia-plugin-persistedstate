822619c93898190cc56afaa919663864fef1c24b
Prazdevs <hi@praz.dev>
2024-08-30 20:02:43 +0200

    feat: nuxt plugin + storages

---
+++ src/runtime/plugin.ts
@@ -0,0 +1,31 @@
+import type { Pinia, PiniaPluginContext } from 'pinia'
+import { destr } from 'destr'
+import type { Persistence } from '../types'
+import { createPersistence } from '../core'
+import { storages } from './storages'
+
+import { defineNuxtPlugin, useNuxtApp } from '#app'
+
+function piniaPlugin(context: PiniaPluginContext) {
+  const nuxtApp = useNuxtApp()
+
+  createPersistence(context, p => ({
+    key: p.key ?? context.store.$id,
+    debug: p.debug,
+    serializer: p.serializer ?? {
+      serialize: data => JSON.stringify(data),
+      deserialize: data => destr(data),
+    },
+    storage: p.storage ?? storages.cookies,
+    beforeHydrate: p.beforeHydrate,
+    afterHydrate: p.afterHydrate,
+    beforePersist: p.beforePersist,
+    afterPersist: p.afterPersist,
+    pick: p.pick,
+    omit: p.omit,
+  } satisfies Persistence), nuxtApp.runWithContext)
+}
+
+export default defineNuxtPlugin(({ $pinia }) => {
+  ($pinia as Pinia).use(piniaPlugin)
+})
