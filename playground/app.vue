0221bfe0c347913838e7467c62687dcb46d97a00
Prazdevs <hi@praz.dev>
2024-08-30 21:30:54 +0200

    chore(playground): improve readability

--- playground/app.vue
+++ playground/app.vue
@@ -6,17 +6,46 @@ const sessionStorageStore = useSessionStorageStore()
 
 <template>
   <main>
-    <label>
-      {{ cookieStore.$id }}
-      <input v-model="cookieStore.username">
-    </label>
-    <label>
-      {{ localStorageStore.$id }}
-      <input v-model="localStorageStore.username">
-    </label>
-    <label>
-      {{ sessionStorageStore.$id }}
-      <input v-model="sessionStorageStore.username">
-    </label>
+    <label for="cookie">{{ cookieStore.$id }}</label>
+    <input id="cookie" v-model="cookieStore.username">
+    <label for="local">{{ localStorageStore.$id }}</label>
+    <input id="locale" v-model="localStorageStore.username">
+    <label for="session">{{ sessionStorageStore.$id }}</label>
+    <input id="session" v-model="sessionStorageStore.username">
   </main>
 </template>
+
+<style>
+body {
+  font-size: 20px;
+  font-family: sans-serif;
+  background-color: #181825;
+  color: #cdd6f4;
+}
+
+main {
+  max-width: 600px;
+  padding: 10px;
+  display: grid;
+  grid-template-columns: auto 1fr;
+  gap: 10px;
+}
+
+label {
+  margin: auto 0 auto auto;
+}
+
+input {
+  border-radius: 5px;
+  border-width: 2px;
+  border-style: solid;
+  border-color: #585b70;
+  background-color: transparent;
+  color: inherit;
+  font-size: inherit;
+}
+
+input:focus-visible {
+  outline: #cba6f7 2px solid;
+}
+</style>
